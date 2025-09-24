84651ee08c4a1d10a44265710f209e78b6d2c249
Pandora-DAW <marycarmencentroseneca@gmail.com>
2025-09-24 13:56:20 +0200

    Curso Laravel
    Avances hasta la lección 15 con cambios en Providers y Routes de web.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,22 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
+
+    // Definimos un patrón global para las rutas que usen {id}, de modo que solo acepten números
+    // Esto afecta a todas las rutas que usen {id} en su definición
+    // Si una ruta usa {id} y se le pasa un valor que no es numérico, Laravel devolverá un error 404 automáticamente
+    // Esto es útil para evitar tener que validar manualmente en cada ruta que usa {id}
+    // También mejora la seguridad al evitar inyecciones de código a través de la URL
+    // Podemos definir otros patrones globales para otros parámetros de ruta si es necesario
+    // Por ejemplo, podríamos definir un patrón para {slug} que solo acepte letras, números y guiones
+    // Route::pattern('slug', '[a-zA-Z0-9-]+');
+    // Esto se hace en el método boot
+    // Recuerda que los patrones globales se aplican a todas las rutas que usan el mismo parámetro
+    
+
     public function boot(): void
     {
-        //
+        Route::pattern('id', '[0-9]+');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,55 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return "Hola desde la página de inicio";
+
+  //  return view('welcome');
 });
+
+
+Route::get('/contacto', function () {
+
+    return "Hola desde la página de contacto usando el método GET";
+
+});
+
+/* AQUÍ DEJA CONSTANCIA DE COMO UTILIZAR EL MÉTODO POST QUE NOS RECABARÍA INFORMACIÓN DE UN FORMULARIO, PERO DE MOMENTO NO LO VAMOS A USAR
+
+Route::post('/contacto', function () {
+
+    return "Hola desde la página de contacto usando el método POST";
+
+});
+
+ASÍ MISMO TAMBIÉN NOS MUESTRA COMO EMPLEAR VARIOS MÉTODOS EN UNA SOLA RUTA, PERO DE MOMENTO NO LO VAMOS A USAR, POR ESO LA DEJO COMENTADA Y NO LA ELIMINO, PARA QUE ME SIRVA DE APUNTES
+
+
+Route::match(['get', 'post'], '/contacto', function () {
+
+    return "Hola desde la página de contacto usando el método GET o POST";
+
+});
+*/
+Route::get('cursos/informacion', function () {
+  
+    return "Aquí encontrarás toda la información de nuestros cursos";
+});
+
+
+Route::get('/cursos/{curso}', function ($curso) {
+
+    return "Bienvenido al curso: $curso";
+});
+
+/* ESTA ES UNA EXPLICACIÓN DE COMO USAR VARIAS CONDICIONES EN UNA PETICIÓN
+PERO VAMOS A USAR DE MOMENTO LA OPCIÓN ANTERIOR
+
+Route::get('/cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if ($categoria) {
+        return "Bienvenido al curso: $curso de la categoría: $categoria";
+    } else {
+        return "Bienvenido al curso: $curso";
+    }
+});
+*/
