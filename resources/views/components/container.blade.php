affc7b1db0a959c865b2c540c45c2556cf70356d
Pandora-DAW <marycarmencentroseneca@gmail.com>
2025-10-02 12:22:02 +0200

    Creacion del layout principal de la aplicacion con componentes desde la consola
    Pendiente depurar la carga de la web

--- resources/views/components/container.blade.php
+++ resources/views/components/container.blade.php
@@ -1,19 +1,17 @@
 @props(['width' => '7xl'])
 
+{{-- 
 @php
 switch ($width) {
     case '7xl':
-        $width = 'max-w-7xl';
-          
-        break;
-    
+        $width = 'max-w-7xl';          
+        break;    
     default:
         # code...
         break;
-}
-
-    
+}    
 @endphp
+--}}
 
 
 <div {{ $attributes->merge(['class' => $width . ' max-w- mx-auto px-4 sm:px-6 lg:px-8']) }}>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Coders Free</title>
+<x-app-layout>
+    
+    <x-slot name="title">
+        Posts
+    </x-slot>
 
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-
-</head>
-<body>
     <x-container width= "7xl">
         <x-alert class="mb-32" type="danger">
-            <x-slot name="title">
+            <x-slot >
                 Alert
             </x-slot>
             Error en su formulario
         </x-alert>
-        <p>Hola mundo</p>
+       
     </x-container>
 
     
-</body>
-</html>
+</x-app-layout>
+path: resources/views/components/app-layout.blade.php
