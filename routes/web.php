6526c307c97ddb5e2a63fffea10bb76ef890d64c
Pandora-DAW <marycarmencentroseneca@gmail.com>
2025-10-03 13:56:34 +0200

    Usando factory y seeders para explicar consultas, sin entrar en muchos detalles. Más tarde lo repasaré.

--- routes/web.php
+++ routes/web.php
@@ -2,78 +2,15 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
-   
+/* Route::prefix('posts')->name('posts.')->controller(PostController::class)->group(function(){}); */
 
-  //  return view('welcome');
+Route::resource('posts', PostController::class);
 
-
-
-Route::get('/contacto', function () {
-
-    return "Hola desde la página de contacto usando el método GET";
-
-});
-
-/* AQUÍ DEJA CONSTANCIA DE COMO UTILIZAR EL MÉTODO POST QUE NOS RECABARÍA INFORMACIÓN DE UN FORMULARIO, PERO DE MOMENTO NO LO VAMOS A USAR
-
-Route::post('/contacto', function () {
-
-    return "Hola desde la página de contacto usando el método POST";
-
-});
-
-ASÍ MISMO TAMBIÉN NOS MUESTRA COMO EMPLEAR VARIOS MÉTODOS EN UNA SOLA RUTA, PERO DE MOMENTO NO LO VAMOS A USAR, POR ESO LA DEJO COMENTADA Y NO LA ELIMINO, PARA QUE ME SIRVA DE APUNTES
-
-
-Route::match(['get', 'post'], '/contacto', function () {
-
-    return "Hola desde la página de contacto usando el método GET o POST";
-
-});
-//COMENTO PARA OBSERVAR LOS CAMBIOS
-
-Route::get('cursos/informacion', function () {
-  
-    return "Aquí encontrarás toda la información de nuestros cursos";
-});
-
-
-Route::get('/cursos/{curso}', function ($curso) {
-
-    return "Bienvenido al curso: $curso";
-});
-
-/* ESTA ES UNA EXPLICACIÓN DE COMO USAR VARIAS CONDICIONES EN UNA PETICIÓN
-PERO VAMOS A USAR DE MOMENTO LA OPCIÓN ANTERIOR
-
-Route::get('/cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
-    if ($categoria) {
-        return "Bienvenido al curso: $curso de la categoría: $categoria";
-    } else {
-        return "Bienvenido al curso: $curso";
-    }
-});
-*/
-
-// DEFINICIÓN DE RUTAS PARA EL CRUD
-//REUTILIZACIÓN PARA MOSTRAR EL AGRUPAMIENTO DE RUTAS
-/*
-Route::controller(PostController::class)->group(function () {
-    Route::get('/post', 'index')->name('post.index');
-    Route::get('/post/create', 'create')->name('post.create');
-    Route::post('/post', 'store')->name('post.store');
-    Route::get('/post/{post}', 'show')->name('post.show');
-    Route::get('/post/{post}/edit', 'edit')->name('post.edit');
-    Route::put('/post/{post}', 'update')->name('post.update');
-    Route::delete('/post/{post}', 'destroy')->name('post.destroy');
-});
-*/
-
-// SIMPLIFICANDO AÚN MÁS EL AGRUPAPIENTO DE RUTAS
 Route::prefix('post')->name('post.')->controller(PostController::class)->group(function () {
     Route::get('/', 'index')->name('index');
     Route::get('/create', 'create')->name('create');
@@ -84,34 +21,11 @@ Route::prefix('post')->name('post.')->controller(PostController::class)->group(f
     Route::delete('/{post}', 'destroy')->name('destroy');
 });
 
+Route::get('/prueba', function () {
+    $users = DB::table('users')->get();
+    return $users;
+});
 
-/*
-//Ruta para mostrar el listado de registros
-Route::get('/post', [PostController::class, 'index'])->name('post.index');
 
-//Ruta para mostrar un formulario para crear un registro
-Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
 
-//Ruta para guardar un registro
-Route::post('/post', [PostController::class, 'store'])->name('post.store');
-
-//Ruta para mostrar un registro
-Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
-
-//Ruta para mostrar un formulario para editar un registro
-Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
-
-//Ruta para actualizar un registro
-Route::put('/post/{post}', [PostController::class, 'update'])->name('post.update');
-
-//Ruta para eliminar un registro
-Route::delete('/post/{post}', [PostController::class, 'destroy'])->name('post.destroy');
-*/
-// DEFINICIÓN DE RUTAS PARA EL CRUD USANDO RESOURCE 
-/*
-Route::resource('articulos', PostController::class)
-->parameters(['articulos' => 'post'])
-->names('post');
-*/
-
-// ESTA LÍNEA SUSTITUYE A TODAS LAS ANTERIORES DEL CRUD
+?>
